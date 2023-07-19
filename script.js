4de2f696b98b5accbae98a700c72102bf3b92dd7
Patchigolla H N SSS Vaisali <125901139+VaisaliPatchigolla@users.noreply.github.com>
2023-07-19 10:37:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function baby(){
+    document.getElementById("life").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG7719K94dsxEzDBLp53oYQQyCgS5n8vvSjg&usqp=CAU";
+}
+function adult(){
+    document.getElementById("life").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbah6QqoC2Ujmxr6BeUcIoYyzHnkKyi3zJCA&usqp=CAU";
+}
+function old(){
+    document.getElementById("life").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPXxMNNpYJbZ6QTpoXvAKDnNIO9903Yn2LIg&usqp=CAU";
+}
